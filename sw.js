4a3fd97f0a2eb9418c72a4dfd5c80c2b88f06fcc
John Paul Patigas <johnpaulpatigas@gmail.com>
2024-12-17 19:51:00 +0800

    Initial commit

---
+++ sw.js
@@ -0,0 +1,13 @@
+self.addEventListener("install", (event) => {
+    console.log("Service Worker: Installed");
+    event.waitUntil(
+        caches
+            .open("static-cache")
+            .then((cache) => {
+                return cache.addAll(["./", "./index.html", "./assets/"]);
+            })
+            .catch((error) => {
+                console.error("Caching failed:", error);
+            })
+    );
+});
